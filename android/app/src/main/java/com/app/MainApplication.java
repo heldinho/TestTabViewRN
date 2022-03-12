e6de6e126c81048900b5367910d82a9c6fd58838
Helder <hfpsis25@gmail.com>
2022-03-12 18:48:46 -0300

    fix
    Signed-off-by: Helder <hfpsis25@gmail.com>

--- android/app/src/main/java/com/app/MainApplication.java
+++ android/app/src/main/java/com/app/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import org.reactnative.camera.RNCameraPackage;
-import com.reactlibrary.RNPdfScannerPackage;
 import com.reactnativepagerview.PagerViewPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
--- app/screens/TabViewMain/Tabs/Calls/index.js
+++ app/screens/TabViewMain/Tabs/Calls/index.js
@@ -1,50 +1,17 @@
 import React from 'react';
 import {
   View,
-  Image,
   Text,
   TextInput,
   TouchableWithoutFeedback,
   Keyboard,
-  PermissionsAndroid,
-  Alert,
 } from 'react-native';
-import DocumentScanner from 'react-native-document-scanner';
 
 export default function Calls() {
   const [count, setCount] = React.useState(0);
-  const [state, setState] = React.useState({
-    image: '',
-    initialImage: null,
-    rectangleCoordinates: null,
-    stableCounter: null,
-    lastDetectionType: null,
-  });
-
-  React.useEffect(() => {
-    fnRequestCameraPermission();
-  }, []);
 
   const fnPress = () => {
     setCount(prev => prev + 1);
-  };
-
-  const fnRequestCameraPermission = async () => {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.CAMERA,
-      {
-        title: 'App Permissão de Câmera',
-        message: 'O App precisa de acesso à câmera.',
-        buttonNeutral: 'Pergunte-me depois',
-        buttonNegative: 'Cancelar',
-        buttonPositive: 'OK',
-      }
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      Alert.alert('Atenção!', 'Você pode usar a Câmera');
-    } else {
-      Alert.alert('Atenção!', 'Permissão de Câmera negada');
-    }
   };
 
   return (
@@ -69,31 +36,6 @@ export default function Calls() {
             </Text>
           </View>
         </TouchableWithoutFeedback>
-        <DocumentScanner
-          useBase64
-          onPictureTaken={data =>
-            setState({
-              image: data.croppedImage,
-              initialImage: data.initialImage,
-              rectangleCoordinates: data.rectangleCoordinates,
-            })
-          }
-          overlayColor="rgba(255,130,0, 0.7)"
-          enableTorch={false}
-          brightness={0.3}
-          saturation={1}
-          contrast={1.1}
-          quality={0.5}
-          onRectangleDetect={({ stableCounter, lastDetectionType }) =>
-            setState({ stableCounter, lastDetectionType })
-          }
-          detectionCountBeforeCapture={5}
-          detectionRefreshRateInMS={50}
-        />
-        <Image
-          source={{ uri: `data:image/jpeg;base64,${state.image}` }}
-          resizeMode="contain"
-        />
       </View>
     </View>
   );
